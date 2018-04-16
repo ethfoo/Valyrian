db828151652036900ac1955cd46467773cad2c80
ethfoo <ethfoo@163.com>
2018-04-16 13:15:49 +0800

    first blood

--- java-web-built/build.sh
+++ java-web-built/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 #该脚本
 # 1. 拉取代码
@@ -12,27 +13,36 @@
 #$MAVEN_HOME=/usr/share/maven
 
 #远程GIT代码仓库
-REMOTE_GIT_ADDR=
+#REMOTE_GIT_ADDR='github.com:ethfoo/base-java-web.git'
+REMOTE_GIT_ADDR='https://github.com/ethfoo/base-java-web.git'
 #代码分支
-BRANCH=
+BRANCH='master'
 #构建的MAVEN子模块
-TARGET_MODULE=
+TARGET_MODULE='base-springmvc-spring Webapp'
 #镜像TAG前缀
-IMAGE_PRE_NAME=
+IMAGE_PRE_NAME='valyrian'
 ###################
+
+LOG='/var/log/valyrian'
 
 ######### git拉取代码 ############
 cd /root
-git clone --branch=$(BRANCH) $(REMOTE_GIT_ADDR) workapp
+echo 'cd /root' > ${LOG}
+echo `ls .` >> ${LOG}
+git clone --branch=${BRANCH} ${REMOTE_GIT_ADDR} workapp
+echo 'git clone' >> ${LOG}
+echo `ls .` >> ${LOG}
 cd /root/workapp
 _COMMIT_HASH=`git rev-parse HEAD`
-echo 'git HEAD commit hash:' $(REV_HASH)
 
 ######### maven构建war包 #########
-mvn -T 1.5C clean install -pl ${TARGET_MODULE} -am -Dmaven.test.skip=true
-cd /root/workapp/${TARGET}/target
+mvn clean install -Dmaven.teset.skip=true
+cd /root/workapp/target
+#mvn clean install -pl ${TARGET_MODULE} -am -Dmaven.test.skip=true
+#cd /root/workapp/${TARGET_MODULE}/target
+
 #将war包复制到file目录里，COPY到运行时镜像中
-mv ./*.war builder/files/ROOT.war
+mv ./*.war /root/builder/file/ROOT.war
 
 ####### 修改startup.sh脚本中的环境变量（JVM参数等均以环境变量的方式注入）#############
 # 不同环境配置在startup.sh中以环境变量的方式设置，用户可以在启动容器的时候以传环境变量CONF=XX的方式动态配置，在此设置会置一个默认值
@@ -41,7 +51,10 @@ mv ./*.war builder/files/ROOT.war
 _REV=${COMMIT_HASH:0:8}
 _NOW=`date +%Y%m%d-%H%M%S`
 
-IMAGE_FULL_NAME=$(IMAGE_PRE_NAME)$(TARGET_MODULE):$(_NOW)$(_REV)
+_IMAGE_FULL_NAME=${IMAGE_PRE_NAME}:${_NOW}${_REV}
 ######### 构建镜像 ###############
 cd /root/builder
-docker build -t $(IMAGE_FULL_NAME) .
+echo 'cd /root/builder' >> ${LOG}
+echo `ls .` >> ${LOG}
+echo ${_IMAGE_FULL_NAME} >> ${LOG}
+docker build -t ${_IMAGE_FULL_NAME} .
--- java-web-built/runtime/file/setenv.sh
+++ java-web-built/runtime/file/setenv.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/bash 
+export JAVA_OPTS="-verbose:gc $JAVA_OPTS"
 
-export JAVA_OPTS=
