3a3e9cb6b1d91533f4c999393580006a985a2f37
ethfoo <ethfoo@163.com>
2018-04-21 16:03:55 +0800

    update

--- run-java-web-images.sh
+++ run-java-web-images.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run -v /var/log/valyrian:/var/log/valyrian -v /root/.ssh:/root/.ssh -v ~/.m2/repository:/root/.m2/repository -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker java-web-build:v1
+docker run -v /var/log/valyrian:/var/log/valyrian -v ~/.m2/repository:/root/.m2/repository -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker java-web-build:v1
+#-v /root/.ssh:/root/.ssh 
