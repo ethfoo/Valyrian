40b25ba3e9f7ddfec49b7affba0df4e80cb7b028
ethfoo <ethfoo@163.com>
2018-05-05 18:13:27 +0800

    update image name

--- build-shell/java-web-built/run-java-web-images.sh
+++ build-shell/java-web-built/run-java-web-images.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker run -v /var/log/valyrian:/var/log/valyrian -v ~/.m2/repository:/root/.m2/repository -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker java-web-build:v1 -r git@github.com:ethfoo/base-java-web.git -b master -s -i ethoojava
+docker run -v ~/.m2/repository:/root/.m2/repository -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker valyrian:buildv1 -r git@github.com:ethfoo/base-java-web.git -b master -s -i ethoojava
+#-v /var/log/valyrian:/var/log/valyrian
 #-v /root/.ssh:/root/.ssh 
